804fa83ca618ea289b387b034b304ee4b1ba3566
Max Keire <maxkeire@tutanota.com>
2020-05-13 20:36:04 -0700

    Added missed files.

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -37,3 +37,124 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+SELECT retirement_info.emp_no,
+retirement_info.first_name,
+retirement_info.last_name,
+dept_employees.to_date
+FROM retirement_info
+LEFT JOIN dept_employees
+ON retirement_info.emp_no = dept_employees.emp_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no,
+	first_name,
+	last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01'
+AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+SELECT * FROM retirement_info;
+
+-- Create table for employees with employee numbers
+SELECT	ri.emp_no,
+		ri.first_name,
+		ri.last_name,
+		de.dept_no
+INTO ret_emp_no_info
+FROM retirement_info as ri
+Inner Join dept_employees as de
+ON (ri.emp_no = de.emp_no);
+
+SELECT * FROM ret_emp_no_info
+
+-- Create table to look for retiring sales team members
+CREATE TABLE ret_emp_by_dept as
+		(SELECT	d.dept_no,
+		d.dept_name,
+		reni.first_name,
+		reni.last_name,
+		reni.emp_no
+FROM ret_emp_no_info as reni
+INNER JOIN departments as d
+ON (reni.dept_no = d.dept_no));
+
+SELECT * FROM ret_emp_by_dept;
+
+CREATE TABLE ret_sales_team as
+(SELECT * FROM ret_emp_by_dept
+WHERE dept_name IN ('Sales'));
+
+SELECT * FROM ret_sales_team;
+					
+CREATE TABLE ret_sales_dev_team as
+(SELECT * FROM ret_emp_by_dept
+WHERE dept_name IN ('Development', 'Sales'));
+
+SELECT * FROM ret_sales_dev_team;
